d9764f285a6c65fc7d38befd9dc0676043a9d159
Kyle Gordon <kyle@lodge.glasgownet.com>
2017-06-25 21:49:04 +0100

    Correct handling of MQTT relayState requests

--- src/homie-sonoff-touch.ino
+++ src/homie-sonoff-touch.ino
@@ -3,7 +3,7 @@
 #include <Homie.h>
 
 #define FW_NAME "homie-sonoff-touch"
-#define FW_VERSION "0.0.4"
+#define FW_VERSION "0.0.5alpha"
 
 /* Magic sequence for Autodetectable Binary Upload */
 const char *__FLAGGED_FW_NAME = "\xbf\x84\xe4\x13\x54" FW_NAME "\x93\x44\x6b\xa7\x75";
@@ -12,6 +12,8 @@ const char *__FLAGGED_FW_VERSION = "\x6a\x3f\x3e\x0e\xe1" FW_VERSION "\xb0\x30\x
 
 // LED_PIN controls the LED under the wifi symbol on the front plate.
 // RELAY_PIN controls the relay, *AND* the LED under the touchpad.
+// Scheme is similar to https://github.com/enc-X/sonoff-homie
+// ie, mosquitto_pub -h homeauto.vpn.glasgownet.com -t 'devices/85376d51/relay/relayState/set' -m 'ON'
 
 const int PIN_RELAY = 12;
 const int PIN_LED = 13;
@@ -20,24 +22,25 @@ const int PIN_BUTTON = 0;
 HomieNode relayNode("relay", "relay");
 
 bool RelayHandler(String value) {
-  if (value == "true") {
+  if (value == "ON") {
     digitalWrite(PIN_RELAY, HIGH);
-    Homie.setNodeProperty(relayNode, "on", "true");
+    Homie.setNodeProperty(relayNode, "set", value);
     Serial.println("Relay is on");
-  } else if (value == "false") {
+  } else if (value == "OFF") {
     digitalWrite(PIN_RELAY, LOW);
-    Homie.setNodeProperty(relayNode, "on", "false");
+    Homie.setNodeProperty(relayNode, "set", value);
     Serial.println("Relay is off");
   } else {
+    Serial.print("Unknown value: ");
+    Serial.println(value);
     return false;
   }
-
   return true;
 }
 
 
 void loopHandler() {
-  digitalWrite(PIN_RELAY, HIGH);
+  //digitalWrite(PIN_RELAY, HIGH);
 }
 
 void setup() {
@@ -51,7 +54,7 @@ void setup() {
   Homie.setLedPin(PIN_LED, HIGH); // Status LED
   Homie.setResetTrigger(PIN_BUTTON, LOW, 5000);
   Homie.setLoopFunction(loopHandler);
-  relayNode.subscribe("on", RelayHandler);
+  relayNode.subscribe("relayState", RelayHandler);
   Homie.registerNode(relayNode);
   Homie.setup();
 }
