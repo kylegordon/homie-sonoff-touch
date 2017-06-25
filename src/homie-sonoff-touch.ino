3aff0f61087a2f42c8faac665db5a918c0df58b8
Kyle Gordon <kyle@lodge.glasgownet.com>
2017-06-25 18:45:44 +0100

    Some notes on hardware

--- src/homie-sonoff-touch.ino
+++ src/homie-sonoff-touch.ino
@@ -10,6 +10,9 @@ const char *__FLAGGED_FW_NAME = "\xbf\x84\xe4\x13\x54" FW_NAME "\x93\x44\x6b\xa7
 const char *__FLAGGED_FW_VERSION = "\x6a\x3f\x3e\x0e\xe1" FW_VERSION "\xb0\x30\x48\xd4\x1a";
 /* End of magic sequence for Autodetectable Binary Upload */
 
+// LED_PIN controls the LED under the wifi symbol on the front plate.
+// RELAY_PIN controls the relay, *AND* the LED under the touchpad.
+
 const int PIN_RELAY = 12;
 const int PIN_LED = 13;
 const int PIN_BUTTON = 0;
