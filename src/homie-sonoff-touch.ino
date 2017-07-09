585a2f9e36d63cc4210ce144768f7e5e134dcf6f
Kyle Gordon <kyle@lodge.glasgownet.com>
2017-07-09 21:17:44 +0100

    Rate limit the messages. Needs fine tuning.

--- src/homie-sonoff-touch.ino
+++ src/homie-sonoff-touch.ino
@@ -2,7 +2,7 @@
 #include <Homie.h>
 
 #define FW_NAME "homie-sonoff-touch"
-#define FW_VERSION "0.0.10"
+#define FW_VERSION "0.0.11"
 
 /* Magic sequence for Autodetectable Binary Upload */
 const char *__FLAGGED_FW_NAME = "\xbf\x84\xe4\x13\x54" FW_NAME "\x93\x44\x6b\xa7\x75";
@@ -22,6 +22,11 @@ const int PIN_LED = 13;
 const int PIN_BUTTON = 0;
 
 int function = 0;
+
+// Timer related for dimming delays
+unsigned long previousMillis = 0;
+// Milliseconds to wait per cycle for a held command.
+const int waitInterval = 100;
 
 // Associate the PIN_BUTTON GPIO with the ClickButton library
 ClickButton button1(PIN_BUTTON, LOW, CLICKBTN_PULLUP);
@@ -84,19 +89,22 @@ void loopHandler() {
   // These are repeat events, where the button is being held down.
   // Handle them, but don't reset function back to 0 unless the button is released.
   if ( function < 0 ) {
-    if ( function == -1 ) {
-      Homie.setNodeProperty(buttonNode, "event", "SINGLEHELD", 0);
-      Serial.println("SINGLE LONG click");
-    }
+    if ( millis() - previousMillis >= waitInterval ) {
+      previousMillis = millis();
+      if ( function == -1 ) {
+        Homie.setNodeProperty(buttonNode, "event", "SINGLEHELD", 0);
+        Serial.println("SINGLE LONG click");
+      }
 
-    if ( function == -2 ) {
-      Homie.setNodeProperty(buttonNode, "event", "DOUBLEHELD", 0);
-      Serial.println("DOUBLE LONG click");
-    }
+      if ( function == -2 ) {
+        Homie.setNodeProperty(buttonNode, "event", "DOUBLEHELD", 0);
+        Serial.println("DOUBLE LONG click");
+      }
 
-    if ( function == -3 ) {
-      Homie.setNodeProperty(buttonNode, "event", "TRIPLEHELD", 0);
-      Serial.println("TRIPLE LONG click");
+      if ( function == -3 ) {
+        Homie.setNodeProperty(buttonNode, "event", "TRIPLEHELD", 0);
+        Serial.println("TRIPLE LONG click");
+      }
     }
 
     // Decide whether the button is being held or not
@@ -108,6 +116,8 @@ void loopHandler() {
       Serial.println("Released");
       function = 0;
     }
+  // Rate limit...
+
   }
 
   delay(5);
